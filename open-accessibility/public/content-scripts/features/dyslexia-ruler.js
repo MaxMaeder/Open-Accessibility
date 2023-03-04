1af1a509eaa892f22f2eaa4eb39db70a455ef948
Max Maeder <max.s.maeder@gmail.com>
2023-03-04 15:46:46 -0600

    Added more features

---
+++ open-accessibility/public/content-scripts/features/dyslexia-ruler.js
@@ -0,0 +1,23 @@
+import { removeInj, updateCSS } from "../util/inject.js";
+
+const SPACINGS = ["20px", "25px", "30px", "35px"];
+
+let cssInj;
+
+const fontSize = (value) => {
+  if (!Number.isInteger(value) || value < 0 || value > SPACINGS.length - 1) {
+    removeInj(cssInj);
+    return;
+  }
+
+  const CSS = `
+  p {
+    font-size: ${SPACINGS[value]} !important;
+  }
+  `;
+  console.log(CSS);
+
+  cssInj = updateCSS(cssInj, CSS);
+};
+
+export default fontSize;
--- open-accessibility/public/content-scripts/features/feature-list.js
+++ open-accessibility/public/content-scripts/features/feature-list.js
@@ -1,8 +1,16 @@
+import fontSize from "./font-size.js";
+import lineHeight from "./line-height.js";
 import wordSpacing from "./word-spacing.js";
 
 const FEATURE_LIST = {
   WORD_SPACING: {
     function: wordSpacing,
+  },
+  LINE_HEIGHT: {
+    function: lineHeight,
+  },
+  FONT_SIZE: {
+    function: fontSize,
   },
 };
 
