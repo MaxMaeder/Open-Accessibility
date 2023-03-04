470761f1e40bf2849d76c9555f2671a6c483d197
RahulHi <67343291+RahulHi@users.noreply.github.com>
2023-03-04 15:20:37 -0600

    Added an onClick property to the square button component to fix previous merge errors

--- open-accessibility/src/components/square-button.js
+++ open-accessibility/src/components/square-button.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import { motion } from "framer-motion"
 
 
@@ -7,9 +7,9 @@ export default function SquareButton(props) {
 
     return (
         <motion.div className={"border-slate-800 border-4 rounded-xl h-32 text-black font-medium p-2"
-        + (enabled ? " bg-slate-700 text-white" : "")}
-        onClick={() => setEnabled(!enabled)}
-        whileHover={{scale: 1.1}}
+            + (enabled ? " bg-slate-700 text-white" : "")}
+            onClick={() => { setEnabled(!enabled); props.onClick() }}
+            whileHover={{ scale: 1.1 }}
         >
             {props.text}
             {props.icon}
